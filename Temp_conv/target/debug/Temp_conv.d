27afd5cdda0b5085f74e889c0a52a5fd011aaa92
KRISH PATEL <kalpeshkrish.22.cse@iite.indusuni.ac.in>
2025-01-06 18:17:44 +0530

    hello world

---
+++ Temp_conv/target/debug/Temp_conv.d
@@ -0,0 +1,1 @@
+C:\Users\Krish\ nu\ Laptop\Documents\GitHub\RUST_PRACTICE\Temp_conv\target\debug\Temp_conv.exe: C:\Users\Krish\ nu\ Laptop\Documents\GitHub\RUST_PRACTICE\Temp_conv\src\main.rs
---
+++ Temp_conv/target/debug/deps/Temp_conv.d
@@ -0,0 +1,5 @@
+C:\Users\Krish nu Laptop\Documents\GitHub\RUST_PRACTICE\Temp_conv\target\debug\deps\Temp_conv.exe: src/main.rs
+
+C:\Users\Krish nu Laptop\Documents\GitHub\RUST_PRACTICE\Temp_conv\target\debug\deps\Temp_conv.d: src/main.rs
+
+src/main.rs:
