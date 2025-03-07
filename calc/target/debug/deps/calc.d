cd5d215b4d55e9fe53ea5a1fefac04c2ba310e95
KRISH PATEL <kalpeshkrish.22.cse@iite.indusuni.ac.in>
2025-03-07 17:28:35 +0530

    something

--- calc/target/debug/deps/calc.d
+++ calc/target/debug/deps/calc.d
@@ -1,5 +1,5 @@
-C:\Users\Krish nu Laptop\Documents\GitHub\RUST_PRACTICE\calc\target\debug\deps\calc.exe: src/main.rs
+C:\Users\Krish nu Laptop\Documents\GitHub\RUST_PRACTICE\calc\target\debug\deps\calc.exe: src\main.rs
 
-C:\Users\Krish nu Laptop\Documents\GitHub\RUST_PRACTICE\calc\target\debug\deps\calc.d: src/main.rs
+C:\Users\Krish nu Laptop\Documents\GitHub\RUST_PRACTICE\calc\target\debug\deps\calc.d: src\main.rs
 
-src/main.rs:
+src\main.rs:
